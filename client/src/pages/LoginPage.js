523522f9a38df6af08c8a68a8328faa65e399d5e
Paulos Teshome Hailemeskel <paulman7792@gmail.com>
2024-12-29 14:55:25 +0300

    Add login page and validation

--- client/src/pages/LoginPage.js
+++ client/src/pages/LoginPage.js
@@ -1,7 +1,114 @@
-import React from "react";
+import { useTheme } from '@emotion/react';
+import { Box, Button, Divider, Stack, TextField, Typography } from '@mui/material';
+import React from 'react';
+import { tokens } from '../theme';
+import AASTULogo from '../assets/AASTU_Logo.svg';
+import { Person } from '@mui/icons-material';
+import PasswordComponent from '../components/form/PasswordComponent';
+import { useFormik } from 'formik';
+import { loginSchema } from '../utils/yupSchemas';
 
 function LoginPage() {
-    return <div>LoginPage</div>;
+	const theme = useTheme();
+	const colors = tokens(theme.palette.mode);
+	const handleLogin = () => {};
+
+	const { values, errors, handleSubmit, handleBlur, handleChange, touched } = useFormik({
+		initialValues: {
+			username: '',
+			password: '',
+		},
+		validationSchema: loginSchema,
+		onSubmit: handleLogin,
+	});
+
+	return (
+		<Box
+			width="100%"
+			height="100vh"
+			sx={{
+				display: 'flex',
+				flexDirection: 'column',
+				alignItems: 'center',
+				justifyContent: 'center',
+				boxShadow: 1,
+				padding: '2rem',
+				borderRadius: '8px',
+			}}
+		>
+			<Stack
+				bgcolor={colors.grey[100]}
+				boxShadow={5}
+				direction="row"
+				display="flex"
+				justifyContent="center"
+				alignItems="center"
+				width="65%"
+				height="60%"
+				borderRadius="5px"
+				p={4}
+				gap={1}
+			>
+				<Stack direction="column" display="flex" justifyContent="center" alignItems="center">
+					<img src={AASTULogo} alt="Not Found" style={{ maxHeight: '150px', width: '40%', height: 'auto' }} />
+					<Typography color={colors.textBlue[500]} variant="h4" component="h1" fontWeight="bold">
+						Strategic Planning Management System
+					</Typography>
+				</Stack>
+				<Divider
+					orientation="vertical"
+					flexItem
+					sx={{ borderRightWidth: 2, borderColor: colors.aastuBlue[500] }}
+				/>
+				<Stack
+					direction="column"
+					display="flex"
+					justifyContent="center"
+					alignItems="center"
+					gap={2}
+					p={1}
+					width="50%"
+					component="form"
+					onSubmit={handleSubmit}
+					autoComplete="off"
+					noValidate
+				>
+					<Stack direction="row" display="flex" justifyContent="center" alignItems="center" gap={2}>
+						<Person />
+						<Typography color={colors.textBlue[500]} variant="h4" component="h1" fontWeight="bold">
+							Login
+						</Typography>
+					</Stack>
+					<TextField
+						name="username"
+						label="Username"
+						type="text"
+						fullWidth
+						value={values.username}
+						onChange={handleChange}
+						onBlur={handleBlur}
+						error={touched.username && !!errors.username}
+						helperText={touched.username && errors.username}
+					/>
+
+					<PasswordComponent
+						name="password"
+						label="Password"
+						fullWidth
+						value={values.password}
+						onChange={handleChange}
+						onBlur={handleBlur}
+						error={touched.password && !!errors.password}
+						helperText={touched.password && errors.password}
+					/>
+
+					<Button type="submit" variant="contained" color="primary" fullWidth size="large">
+						Login
+					</Button>
+				</Stack>
+			</Stack>
+		</Box>
+	);
 }
 
 export default LoginPage;
--- client/src/utils/yupSchemas.js
+++ client/src/utils/yupSchemas.js
@@ -1,4 +1,4 @@
-import * as yup from "yup";
+import * as yup from 'yup';
 
 // const phoneRegExp = /^((\+[1-9]{1,4}[ -]?)|(\([0-9]{2,3}\)[ -]?)|([0-9]{2,4})[ -]?)*?[0-9]{3,4}[ -]?[0-9]{3,4}$/;
 // const phoneRegExp = /^\d{9}$/;
@@ -6,13 +6,10 @@ const phoneRegExp = /^[97]\d{8}$/;
 const passwordRules = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}$/;
 
 export const loginSchema = yup.object().shape({
-    staff_id: yup.string().required("Staff id  is required"),
-    password: yup.string().required("Password is required"),
+	username: yup.string().required('Username is required'),
+	password: yup.string().required('Password is required'),
 });
 
 export const scheduleSchema = yup.object().shape({
-    schedule_date: yup
-        .date()
-        .typeError("Please specify a valid date!")
-        .required("required!"),
+	schedule_date: yup.date().typeError('Please specify a valid date!').required('required!'),
 });
