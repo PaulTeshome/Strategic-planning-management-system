399ec7ee8021c531fc4ddde705b8d75e2a5bd231
Paulos Teshome Hailemeskel <paulman7792@gmail.com>
2024-12-30 04:40:39 +0300

    Update VPplan.js
    insert search form

--- client/src/pages/president/VPplan.js
+++ client/src/pages/president/VPplan.js
@@ -1,7 +1,94 @@
+import { useTheme } from '@emotion/react';
+import { Button, Grid2, Stack, TextField, Typography } from '@mui/material';
 import React from 'react';
+import { tokens } from '../../theme';
+import { useFormik } from 'formik';
+import SelectComponent from '../../components/form/SelectComponent';
+import { vpPlanSchema } from '../../utils/yupSchemas';
 
 function VPplan() {
-	return <div>VPplan</div>;
+	const theme = useTheme();
+	const colors = tokens(theme.palette.mode);
+
+	const handleSearch = () => {};
+
+	const { values, errors, handleSubmit, handleBlur, handleChange, touched } = useFormik({
+		initialValues: {
+			year: '',
+			department: '',
+		},
+		validationSchema: vpPlanSchema,
+		onSubmit: handleSearch,
+	});
+
+	return (
+		<Stack
+			direction="column"
+			display="flex"
+			justifyContent="space-between"
+			bgcolor={colors.grey[100]}
+			width="100%"
+			minHeight={150}
+			borderRadius="5px"
+			boxShadow={5}
+			p={2}
+			px={5}
+			gap={2}
+		>
+			<Typography variant="h5" component="p" fontWeight="bold">
+				View Plans
+			</Typography>
+			<Grid2
+				container
+				display="flex"
+				alignItems="center"
+				width="65%"
+				gap={1}
+				component="form"
+				onSubmit={handleSubmit}
+				autoComplete="off"
+				noValidate
+			>
+				<Grid2 size={{ xs: 3 }}>
+					<TextField
+						name="year"
+						label="Year"
+						type="number"
+						fullWidth
+						value={values.year}
+						onChange={handleChange}
+						onBlur={handleBlur}
+						error={touched.year && !!errors.year}
+						helperText={touched.year && errors.year}
+					/>
+				</Grid2>
+				<Grid2 size={{ xs: 4 }}>
+					<SelectComponent
+						required={true}
+						touched={touched.department}
+						error={errors.department}
+						label="Department*"
+						name="department"
+						value={values.department}
+						onChange={handleChange}
+						onBlur={handleBlur}
+						options={[
+							{ value: 'all', label: 'All' },
+							{ value: 'academics', label: 'Academics Office' },
+							{ value: 'president', label: 'President Office' },
+							{ value: 'research', label: 'Research Office' },
+							{ value: 'administration', label: 'Administration Office' },
+						]}
+					/>
+				</Grid2>
+				<Grid2 size={{ xs: 2 }} display="flex" alignItems="center">
+					<Button type="submit" fullWidth variant="contained" size="large">
+						Search Plan
+					</Button>
+				</Grid2>
+			</Grid2>
+		</Stack>
+	);
 }
 
 export default VPplan;
