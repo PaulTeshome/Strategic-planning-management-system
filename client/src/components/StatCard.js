87d245a5e883f892c6c1020704e1bd5bf9259bfe
Paulos Teshome Hailemeskel <paulman7792@gmail.com>
2024-12-30 03:23:15 +0300

    Create StatCard.js

---
+++ client/src/components/StatCard.js
@@ -0,0 +1,20 @@
+import { Stack, Typography } from '@mui/material';
+import React from 'react';
+
+function StatCard({ icon, title, data }) {
+	return (
+		<Stack direction="row" gap={2}>
+			{icon}
+			<Stack direction="column">
+				<Typography variant="h3" component="p" fontWeight="bold">
+					{title}
+				</Typography>
+				<Typography variant="h6" component="p">
+					{data}
+				</Typography>
+			</Stack>
+		</Stack>
+	);
+}
+
+export default StatCard;
