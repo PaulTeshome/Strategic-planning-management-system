2e1e13b2d75584a8d646b990d9efbc497dfa92b2
Paulos Teshome Hailemeskel <paulman7792@gmail.com>
2024-12-31 23:53:46 +0300

    Update viewplantable styling and row render

--- client/src/components/tables/ViewPlanTable.js
+++ client/src/components/tables/ViewPlanTable.js
@@ -23,7 +23,7 @@ function ViewPlanTable({ columns, rows }) {
 			color: 'white',
 			fontWeight: 'bold',
 			fontSize: '10px',
-			borderRight: '1px solid white',
+			borderRight: `1px solid ${colors.grey[700]}`,
 		},
 	}));
 	// const StyledTableCell = styled(TableCell)(({ theme }) => ({
@@ -46,65 +46,92 @@ function ViewPlanTable({ columns, rows }) {
 		[`&.${tableCellClasses.body}`]: {
 			fontWeight: 'bold',
 			fontSize: '10px',
+			borderRight: `1px solid ${colors.grey[700]}`,
 		},
 	}));
 	const TableBodyCell = styled(TableCell)(({ theme }) => ({
 		[`&.${tableCellClasses.body}`]: {
 			fontSize: '10px',
+			borderRight: `1px solid ${colors.grey[700]}`,
 		},
 	}));
 	const StyledTableRow = styled(TableRow)(({ theme }) => ({
-		'&:nth-of-type(even)': {
-			backgroundColor: 'lightblue',
-		},
-		// hide last border
-		'&:hover': {
-			backgroundColor: '#4CAFB2',
-		},
-		'&:last-child td, &:last-child th': {
-			border: 0,
-		},
 		'&.goal': {
 			backgroundColor: colors.aastuGold[500],
 		},
+		'&.main': {
+			backgroundColor: colors.textBlue[200],
+		},
+		'&.detail': {
+			backgroundColor: colors.grey[300],
+		},
+		'&.kpi': {
+			backgroundColor: colors.grey[100],
+		},
 	}));
+
+	const renderTableRows = (data) => {
+		const rows = [];
+
+		const renderRow = (item, level = 0) => {
+			const isKPI = item.KPI_title !== undefined;
+
+			let className = '';
+			if (item.KPI_title) {
+				className = 'kpi';
+			} else if (item.detail_func_title) {
+				className = 'detail';
+			} else if (item.main_func_title) {
+				className = 'main';
+			} else if (item.main_goal) {
+				className = 'goal';
+			}
+
+			rows.push(
+				<StyledTableRow key={item.number} className={className}>
+					<FirstColTableCell style={{ paddingLeft: `${level * 2 + 10}px` }}>{item.number}</FirstColTableCell>
+					<TableBodyCell style={{ paddingLeft: `${level * 2 + 10}px` }}>
+						{item.main_goal || item.main_func_title || item.detail_func_title || item.KPI_title}
+					</TableBodyCell>
+					<TableBodyCell>{isKPI ? item.weight : item.weight || ''}</TableBodyCell>
+					<TableBodyCell>{isKPI ? item.measurement : ''}</TableBodyCell>
+					<TableBodyCell>{isKPI ? item.past_year : ''}</TableBodyCell>
+					<TableBodyCell>{isKPI ? item.present_goal : ''}</TableBodyCell>
+					<TableBodyCell>{isKPI ? item.quarter_1 : ''}</TableBodyCell>
+					<TableBodyCell>{isKPI ? item.quarter_2 : ''}</TableBodyCell>
+					<TableBodyCell>{isKPI ? item.quarter_3 : ''}</TableBodyCell>
+					<TableBodyCell>{isKPI ? item.quarter_4 : ''}</TableBodyCell>
+					<TableBodyCell>{isKPI ? item.department : ''}</TableBodyCell>
+				</StyledTableRow>
+			);
+
+			if (item.main_functions) {
+				item.main_functions.forEach((func) => renderRow(func, level + 1));
+			}
+			if (item.detail_functions) {
+				item.detail_functions.forEach((detailFunc) => renderRow(detailFunc, level + 2));
+			}
+			if (item.KPIs) {
+				item.KPIs.forEach((kpi) => renderRow(kpi, level + 3));
+			}
+		};
+
+		data.forEach((item) => renderRow(item));
+		return rows;
+	};
 	return (
 		<TableContainer component={Paper}>
 			<Table size="small" stickyHeader sx={{ tableLayout: 'auto' }} aria-label="ANC table">
 				<TableHead>
 					<StyledTableRow>
-						<GroupedTableCell align="center" colSpan={13}>
-							ANC Contact
-						</GroupedTableCell>
-						<GroupedTableCell align="center" colSpan={6}>
-							HIV assessment and follow up
-						</GroupedTableCell>
-						<GroupedTableCell align="center" colSpan={4}>
-							Partner test
-						</GroupedTableCell>
-						<GroupedTableCell align="center" colSpan={5}>
-							Counseling on
-						</GroupedTableCell>
+						{columns.map((column, i) => (
+							<GroupedTableCell key={i} align="center" colSpan={column.colSpan}>
+								{column.name}
+							</GroupedTableCell>
+						))}
 					</StyledTableRow>
 				</TableHead>
-				<TableBody>
-					<StyledTableRow
-						className="goal"
-						// // onClick={() => {
-						// // 	setVisitForm(index);
-						// // }}
-						// // key={index}
-						// sx={{
-						// 	'&:last-child td, &:last-child th': { border: 0 },
-						// 	'&:hover': {
-						// 		cursor: 'pointer',
-						// 	},
-						// }}
-					>
-						<FirstColTableCell>hello</FirstColTableCell>
-						<TableBodyCell>there</TableBodyCell>
-					</StyledTableRow>
-				</TableBody>
+				<TableBody>{renderTableRows(rows)}</TableBody>
 			</Table>
 		</TableContainer>
 	);
