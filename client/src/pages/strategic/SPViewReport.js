b255311a84019335d7b79311f2f29fc9c45f6805
Paulos Teshome Hailemeskel <paulman7792@gmail.com>
2025-01-15 00:22:12 +0300

    Update SPViewReport.js
    fix get row id

--- client/src/pages/strategic/SPViewReport.js
+++ client/src/pages/strategic/SPViewReport.js
@@ -1,5 +1,5 @@
 import { useTheme } from '@emotion/react';
-import { Box, Button,  Stack, Typography } from '@mui/material';
+import { Box, Button, Stack, Typography } from '@mui/material';
 import React, { useState } from 'react';
 import { tokens } from '../../theme';
 import { DataGrid } from '@mui/x-data-grid';
@@ -7,8 +7,7 @@ import DataGridWrapper from '../../components/global/DataGridWrapper';
 import CustomToolbar from '../../components/global/CustomToolbar';
 import { Link } from 'react-router-dom';
 
-
-import { Grid2,  TextField } from '@mui/material';
+import { Grid2, TextField } from '@mui/material';
 import { useFormik } from 'formik';
 import SelectComponent from '../../components/form/SelectComponent';
 import { vpPlanSchema } from '../../utils/yupSchemas';
@@ -17,7 +16,7 @@ import ConfirmationModal from '../../components/modals/ConfirmationModal';
 
 function SPViewReport() {
 	const theme = useTheme();
-    const colors = tokens(theme.palette.mode);
+	const colors = tokens(theme.palette.mode);
 
 	const [confirmOpen, setConfirmOpen] = useState(false);
 
@@ -39,47 +38,46 @@ function SPViewReport() {
 		validationSchema: vpPlanSchema,
 		onSubmit: handleSearch,
 	});
-	
 
 	const [rows, setRows] = useState([
 		{
 			report_id: '1',
 			report_date: '12-12-2024',
 			department: 'Academics',
-			year:"2017",
-            quarter: '1st',
+			year: '2017',
+			quarter: '1st',
 			submitted_by: 'Abebe Bekele',
 		},
 		{
 			report_id: '2',
 			report_date: '12-12-2024',
 			department: 'Academics',
-			year:"2017",
-            quarter: '1st',
+			year: '2017',
+			quarter: '1st',
 			submitted_by: 'Abebe Bekele',
 		},
 		{
 			report_id: '3',
 			report_date: '12-12-2024',
 			department: 'Academics',
-			year:"2017",
-            quarter: '1st',
+			year: '2017',
+			quarter: '1st',
 			submitted_by: 'Abebe Bekele',
 		},
 		{
 			report_id: '4',
 			report_date: '12-12-2024',
 			department: 'Academics',
-			year:"2017",
-            quarter: '1st',
+			year: '2017',
+			quarter: '1st',
 			submitted_by: 'Abebe Bekele',
 		},
 		{
 			report_id: '5',
 			report_date: '12-12-2024',
 			department: 'Academics',
-			year:"2017",
-            quarter: '1st',
+			year: '2017',
+			quarter: '1st',
 			submitted_by: 'Abebe Bekele',
 		},
 	]);
@@ -104,7 +102,7 @@ function SPViewReport() {
 			headerName: 'Year',
 			flex: 1,
 		},
-        {
+		{
 			field: 'quarter',
 			headerName: 'Quarter',
 			flex: 1,
@@ -121,7 +119,12 @@ function SPViewReport() {
 			flex: 1,
 			renderCell: (params) => (
 				<Stack direction="row" width="100%" height="100%" display="flex" alignItems="center">
-					<Button component={Link} variant="outlined" size="small" to={`/strategic/report/${params.row.report_id}`}>
+					<Button
+						component={Link}
+						variant="outlined"
+						size="small"
+						to={`/strategic/report/${params.row.report_id}`}
+					>
 						View
 					</Button>
 				</Stack>
@@ -132,106 +135,106 @@ function SPViewReport() {
 	return (
 		<Box display="flex" flexDirection="column" width="100%" height="100%" gap={2}>
 			<Stack
-			direction="column"
-			display="flex"
-			justifyContent="space-between"
-			bgcolor={colors.grey[100]}
-			width="100%"
-			minHeight={150}
-			borderRadius="5px"
-			boxShadow={5}
-			p={2}
-			px={5}
-			gap={2}
-		>
-			<Typography variant="h5" component="p" fontWeight="bold">
-				View Plans
-			</Typography>
-			<Grid2
-				container
+				direction="column"
 				display="flex"
-				justifyContent="center"
-				alignItems="center"
+				justifyContent="space-between"
+				bgcolor={colors.grey[100]}
 				width="100%"
-				gap={1}
-				component="form"
-				onSubmit={handleSubmit}
-				autoComplete="off"
-				noValidate
+				minHeight={150}
+				borderRadius="5px"
+				boxShadow={5}
+				p={2}
+				px={5}
+				gap={2}
 			>
-				<Grid2 size={{ xs: 3 }}>
-					<TextField
-						name="year"
-						label="Year"
-						type="number"
-						fullWidth
-						value={values.year}
-						onChange={handleChange}
-						onBlur={handleBlur}
-						slotProps={{
-							htmlInput: {
-								min: 1,
-							},
-						}}
-						error={touched.year && !!errors.year}
-						helperText={touched.year && errors.year}
-					/>
+				<Typography variant="h5" component="p" fontWeight="bold">
+					View Plans
+				</Typography>
+				<Grid2
+					container
+					display="flex"
+					justifyContent="center"
+					alignItems="center"
+					width="100%"
+					gap={1}
+					component="form"
+					onSubmit={handleSubmit}
+					autoComplete="off"
+					noValidate
+				>
+					<Grid2 size={{ xs: 3 }}>
+						<TextField
+							name="year"
+							label="Year"
+							type="number"
+							fullWidth
+							value={values.year}
+							onChange={handleChange}
+							onBlur={handleBlur}
+							slotProps={{
+								htmlInput: {
+									min: 1,
+								},
+							}}
+							error={touched.year && !!errors.year}
+							helperText={touched.year && errors.year}
+						/>
+					</Grid2>
+					<Grid2 size={{ xs: 4 }}>
+						<SelectComponent
+							required={true}
+							touched={touched.department}
+							error={errors.department}
+							label="Department*"
+							name="department"
+							value={values.department}
+							onChange={handleChange}
+							onBlur={handleBlur}
+							options={[
+								{ value: 'all', label: 'All' },
+								{ value: 'academics', label: 'Academics Office' },
+								{ value: 'president', label: 'President Office' },
+								{ value: 'research', label: 'Research Office' },
+								{ value: 'administration', label: 'Administration Office' },
+							]}
+						/>
+					</Grid2>
+					<Grid2 size={{ xs: 2 }} display="flex" maxHeight="fit-content">
+						<Button type="submit" fullWidth variant="contained" size="large">
+							Search Plan
+						</Button>
+					</Grid2>
+					<Grid2 size={{ xs: 9 }} display="flex" maxHeight="fit-content">
+						<Typography variant="h6" component="p" color={colors.textBlue[500]}>
+							Plan for {values.year}
+						</Typography>
+					</Grid2>
+					<Grid2 size={{ xs: 2 }} display="flex" maxHeight="fit-content">
+						<Button
+							fullWidth
+							onClick={() => {
+								setConfirmOpen(true);
+							}}
+							variant="contained"
+							startIcon={<CheckCircle sx={{ textDecorationColor: colors.aastuBlue[500] }} />}
+							sx={{ bgcolor: colors.aastuGold[500], color: colors.aastuBlue[500] }}
+							size="medium"
+						>
+							Approve Plan
+						</Button>
+						<ConfirmationModal
+							open={confirmOpen}
+							onCancel={closeConfirm}
+							onConfirm={() => {
+								handleApprove();
+								setConfirmOpen(false);
+							}}
+							title="Approve Plan"
+							message="Are you sure you want to approve this plan?"
+						/>
+					</Grid2>
 				</Grid2>
-				<Grid2 size={{ xs: 4 }}>
-					<SelectComponent
-						required={true}
-						touched={touched.department}
-						error={errors.department}
-						label="Department*"
-						name="department"
-						value={values.department}
-						onChange={handleChange}
-						onBlur={handleBlur}
-						options={[
-							{ value: 'all', label: 'All' },
-							{ value: 'academics', label: 'Academics Office' },
-							{ value: 'president', label: 'President Office' },
-							{ value: 'research', label: 'Research Office' },
-							{ value: 'administration', label: 'Administration Office' },
-						]}
-					/>
-				</Grid2>
-				<Grid2 size={{ xs: 2 }} display="flex" maxHeight="fit-content">
-					<Button type="submit" fullWidth variant="contained" size="large">
-						Search Plan
-					</Button>
-				</Grid2>
-				<Grid2 size={{ xs: 9 }} display="flex" maxHeight="fit-content">
-					<Typography variant="h6" component="p" color={colors.textBlue[500]}>
-						Plan for {values.year}
-					</Typography>
-				</Grid2>
-				<Grid2 size={{ xs: 2 }} display="flex" maxHeight="fit-content">
-					<Button
-						fullWidth
-						onClick={() => {
-							setConfirmOpen(true);
-						}}
-						variant="contained"
-						startIcon={<CheckCircle sx={{ textDecorationColor: colors.aastuBlue[500] }} />}
-						sx={{ bgcolor: colors.aastuGold[500], color: colors.aastuBlue[500] }}
-						size="medium"
-					>
-						Approve Plan
-					</Button>
-					<ConfirmationModal
-						open={confirmOpen}
-						onCancel={closeConfirm}
-						onConfirm={() => {
-							handleApprove();
-							setConfirmOpen(false);
-						}}
-						title="Approve Plan"
-						message="Are you sure you want to approve this plan?"
-					/>
-				</Grid2>
-			</Grid2>
-            </Stack>
+			</Stack>
 			<Stack
 				direction="column"
 				display="flex"
@@ -250,7 +253,7 @@ function SPViewReport() {
 				</Typography>
 				<DataGridWrapper>
 					<DataGrid
-						getRowId={(row) => row.plan_id}
+						getRowId={(row) => row.report_id}
 						rows={rows}
 						columns={columns}
 						slots={{ toolbar: CustomToolbar }}
