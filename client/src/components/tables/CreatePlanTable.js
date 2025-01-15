765e3f04b06a10a1fb755a3b3edf2060303c3db2
Paulos Teshome Hailemeskel <paulman7792@gmail.com>
2025-01-15 07:07:45 +0300

    Add upto KPI

--- client/src/components/tables/CreatePlanTable.js
+++ client/src/components/tables/CreatePlanTable.js
@@ -1,6 +1,7 @@
 import { useTheme } from '@emotion/react';
 import {
 	Button,
+	Grid2,
 	Paper,
 	styled,
 	Table,
@@ -12,11 +13,13 @@ import {
 	TableRow,
 	TextField,
 } from '@mui/material';
-import React from 'react';
+import React, { useState } from 'react';
 import { tokens } from '../../theme';
 import { Add } from '@mui/icons-material';
+import { useFormik } from 'formik';
+import * as yup from 'yup';
 
-function CreatePlanTable({ columns, rows, formikValue, setRows }) {
+function CreatePlanTable({ year, planData, setPlanData }) {
 	const theme = useTheme();
 	const colors = tokens(theme.palette.mode);
 
@@ -37,12 +40,14 @@ function CreatePlanTable({ columns, rows, formikValue, setRows }) {
 			borderRight: `1px solid ${colors.grey[500]}`,
 		},
 	}));
+
 	const TableBodyCell = styled(TableCell)(({ theme }) => ({
 		[`&.${tableCellClasses.body}`]: {
 			fontSize: '10px',
 			borderRight: `1px solid ${colors.grey[500]}`,
 		},
 	}));
+
 	const StyledTableRow = styled(TableRow)(({ theme }) => ({
 		'&.goal': {
 			backgroundColor: colors.aastuGold[500],
@@ -61,6 +66,64 @@ function CreatePlanTable({ columns, rows, formikValue, setRows }) {
 			'&:hover': { backgroundColor: colors.grey[300] },
 		},
 	}));
+
+	const [rows, setRows] = useState([]);
+	const columns = [
+		{ name: 'Number', colSpan: 1 },
+		{ name: 'Strategic Goals , Main Activities, Detail functions and KPIs', colSpan: 1 },
+		{ name: 'Weights', colSpan: 1 },
+		{ name: 'Measurements', colSpan: 1 },
+		{ name: `Previous year(${year - 1}) value`, colSpan: 1 },
+		{ name: `This year(${year}) Goal`, colSpan: 1 },
+		{ name: 'Quarter 1', colSpan: 1 },
+		{ name: 'Quarter 2', colSpan: 1 },
+		{ name: 'Quarter 3', colSpan: 1 },
+		{ name: 'Quarter 4', colSpan: 1 },
+		{ name: 'Department', colSpan: 1 },
+	];
+
+	const handleSearch = () => {};
+
+	const createPlanSchema = yup.object().shape({
+		year: yup.number().min(1, 'year cannot be negative number').required('Year is required'),
+		department: yup.string().required('Department is required'),
+		plan_document: yup
+			.mixed()
+			.nullable() // Allow null
+			.required('Plan document is required') // Make optional
+			.test(
+				'fileFormat',
+				'Unsupported File Format. Please select a PDF or Word document',
+				(value) =>
+					!value ||
+					[
+						'application/pdf',
+						'application/msword',
+						'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
+					].includes(value?.type)
+			)
+			.test('fileSize', 'File too large. Maximum size is 4MB', (value) => !value || value.size <= 4000000), // Max size 2MB
+	});
+
+	const planDataFormik = useFormik({
+		initialValues: {
+			plan_document: null,
+		},
+		validationSchema: createPlanSchema,
+		onSubmit: handleSearch,
+	});
+
+	const addGoal = () => {
+		const goalNumber = rows.length + 1;
+		const newGoal = {
+			number: `${goalNumber}`,
+			main_goal: `goal ${goalNumber}`,
+			weight: '',
+			main_functions: [],
+		};
+
+		setRows((prev) => [...prev, newGoal]);
+	};
 
 	const addMain = (main_functions, goalNumber) => {
 		const functionNumber = main_functions.length + 1;
@@ -99,20 +162,67 @@ function CreatePlanTable({ columns, rows, formikValue, setRows }) {
 				if (goal.number === goalNumber) {
 					return {
 						...goal,
-						main_functions: goal.main_functions.map((fn) =>
-							fn.number === functionNumber
-								? {
-										...fn,
-										detail_functions: [...fn.detail_functions, newDetailFunction],
-									}
-								: fn
-						),
+						main_functions: goal.main_functions.map((main) => {
+							if (main.number === functionNumber) {
+								return {
+									...main,
+									detail_functions: [...main.detail_functions, newDetailFunction],
+								};
+							}
+							return main;
+						}),
 					};
 				}
 				return goal; // Keep other goals unchanged
 			})
 		);
 	};
+
+	const addKPI = (kpi, detailNumber) => {
+		console.log('🚀 ~ addKPI ~ detailNumber:', detailNumber);
+		const kpiNumber = kpi.length + 1;
+		const [goalNumber, functionNumber] = detailNumber.split('.');
+		console.log(
+			'🚀 ~ addKPI ~ goalNumber, functionNumber, detailNumber:',
+			goalNumber,
+			functionNumber,
+			detailNumber
+		);
+		const newKPI = {
+			number: detailNumber + '.' + kpiNumber,
+			kpi_title: `kpi ${kpiNumber}`,
+			weight: '',
+		};
+
+		setRows((prevRows) =>
+			prevRows.map((goal) => {
+				if (goal.number === goalNumber) {
+					return {
+						...goal,
+						main_functions: goal.main_functions.map((main) => {
+							if (main.number === `${goalNumber}.${functionNumber}`) {
+								return {
+									...main,
+									detail_functions: main.detail_functions.map((detail) => {
+										if (detail.number === detailNumber) {
+											return {
+												...detail,
+												KPIs: [...detail.KPIs, newKPI],
+											};
+										}
+										return detail;
+									}),
+								};
+							}
+							return main;
+						}),
+					};
+				}
+				return goal; // Keep other goals unchanged
+			})
+		);
+	};
+
 	const renderTableRows = (data) => {
 		const rows = [];
 
@@ -152,12 +262,18 @@ function CreatePlanTable({ columns, rows, formikValue, setRows }) {
 							size="small"
 							type="text"
 							label={textLabel + ' Title ' + item.number}
-							name={textLabel + '_title_' + item.number}
-							onBlur={formikValue.handleBlur}
-							onChange={formikValue.handleChange}
-							value={formikValue.values.confirm_password}
-							error={formikValue.touched.confirm_password && Boolean(formikValue.errors.confirm_password)}
-							helperText={formikValue.touched.confirm_password && formikValue.errors.confirm_password}
+							name={textName + '_title_' + item.number}
+							onBlur={planDataFormik.handleBlur}
+							onChange={planDataFormik.handleChange}
+							value={planDataFormik.values[textName + '_title_' + item.number]}
+							error={
+								planDataFormik.touched[textName + '_title_' + item.number] &&
+								Boolean(planDataFormik.errors[textName + '_title_' + item.number])
+							}
+							helperText={
+								planDataFormik.touched[textName + '_title_' + item.number] &&
+								planDataFormik.errors[textName + '_title_' + item.number]
+							}
 						/>
 					</TableBodyCell>
 					<TableBodyCell>
@@ -169,12 +285,18 @@ function CreatePlanTable({ columns, rows, formikValue, setRows }) {
 							size="small"
 							type="number"
 							label={textLabel + ' Weight ' + item.number}
-							name={textLabel + '_weight_' + item.number}
-							onBlur={formikValue.handleBlur}
-							onChange={formikValue.handleChange}
-							value={formikValue.values.confirm_password}
-							error={formikValue.touched.confirm_password && Boolean(formikValue.errors.confirm_password)}
-							helperText={formikValue.touched.confirm_password && formikValue.errors.confirm_password}
+							name={textName + '_weight_' + item.number}
+							onBlur={planDataFormik.handleBlur}
+							onChange={planDataFormik.handleChange}
+							value={planDataFormik.values[textName + '_weight_' + item.number]}
+							error={
+								planDataFormik.touched[textName + '_weight_' + item.number] &&
+								Boolean(planDataFormik.errors[textName + '_weight_' + item.number])
+							}
+							helperText={
+								planDataFormik.touched[textName + '_weight_' + item.number] &&
+								planDataFormik.errors[textName + '_weight_' + item.number]
+							}
 						/>
 					</TableBodyCell>
 					{item.main_goal !== undefined && (
@@ -191,7 +313,7 @@ function CreatePlanTable({ columns, rows, formikValue, setRows }) {
 								Add Main Function
 							</Button>
 						</TableBodyCell>
-					)}{' '}
+					)}
 					{item.main_func_title !== undefined && (
 						<TableBodyCell colSpan={8}>
 							<Button
@@ -204,6 +326,21 @@ function CreatePlanTable({ columns, rows, formikValue, setRows }) {
 								size="small"
 							>
 								Add Detail Function
+							</Button>
+						</TableBodyCell>
+					)}
+					{item.detail_func_title !== undefined && (
+						<TableBodyCell colSpan={8}>
+							<Button
+								onClick={() => {
+									addKPI(item.KPIs, item.number);
+								}}
+								fullWidth
+								variant="text"
+								startIcon={<Add sx={{ textDecorationColor: colors.aastuBlue[500] }} />}
+								size="small"
+							>
+								Add KPI
 							</Button>
 						</TableBodyCell>
 					)}
@@ -237,20 +374,36 @@ function CreatePlanTable({ columns, rows, formikValue, setRows }) {
 		return rows;
 	};
 	return (
-		<TableContainer component={Paper}>
-			<Table size="small" stickyHeader sx={{ tableLayout: 'auto' }} aria-label="ANC table">
-				<TableHead>
-					<StyledTableRow>
-						{columns.map((column, i) => (
-							<GroupedTableCell key={i} align="center" colSpan={column.colSpan}>
-								{column.name}
-							</GroupedTableCell>
-						))}
-					</StyledTableRow>
-				</TableHead>
-				<TableBody>{renderTableRows(rows)}</TableBody>
-			</Table>
-		</TableContainer>
+		<Grid2 container display="flex" justifyContent="center" alignItems="center" width="100%" gap={1}>
+			<Grid2 size={{ xs: 12 }} display="flex" pl="85%" alignItems="flex-end" maxHeight="fit-content">
+				<Button
+					onClick={() => {
+						addGoal();
+					}}
+					fullWidth
+					variant="outlined"
+					startIcon={<Add sx={{ textDecorationColor: colors.aastuBlue[500] }} />}
+					// sx={{ bgcolor: colors.aastuGold[500], color: colors.aastuBlue[500] }}
+					size="medium"
+				>
+					Add Goal
+				</Button>
+			</Grid2>
+			<TableContainer component={Paper}>
+				<Table size="small" stickyHeader sx={{ tableLayout: 'auto' }} aria-label="ANC table">
+					<TableHead>
+						<StyledTableRow>
+							{columns.map((column, i) => (
+								<GroupedTableCell key={i} align="center" colSpan={column.colSpan}>
+									{column.name}
+								</GroupedTableCell>
+							))}
+						</StyledTableRow>
+					</TableHead>
+					<TableBody>{renderTableRows(rows)}</TableBody>
+				</Table>
+			</TableContainer>
+		</Grid2>
 	);
 }
 
