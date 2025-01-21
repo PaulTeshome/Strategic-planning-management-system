fe88ec4192c0cfdd2f4606040e807a41af0978d1
Paulos Teshome Hailemeskel <paulman7792@gmail.com>
2025-01-22 01:41:12 +0300

    Fix Vp sidebar

--- client/src/components/global/Sidebar.js
+++ client/src/components/global/Sidebar.js
@@ -34,16 +34,11 @@ const Sidebar = () => {
 			requiredRole: ['vp'],
 			element: (
 				<Fragment key="pres">
-					<NavItem key="/offices/" title="Dashboard" to="/offices/" icon={<FaDashcube />} />
-					<NavItem key="/offices/plan" title="Plan" to="/offices/plan" icon={<Book />} />
-					<NavItem
-						key="/offices/report"
-						title="Report"
-						to="/offices/report"
-						icon={<BarChartOutlinedIcon />}
-					/>
-					<NavItem key="/offices/schedule" title="Schedule" to="/offices/schedule" icon={<CalendarMonth />} />
-					<NavItem key="/offices/feedback" title="Feedback" to="/offices/feedback" icon={<Chat />} />
+					<NavItem key="/vp/" title="Dashboard" to="/vp/" icon={<FaDashcube />} />
+					<NavItem key="/vp/plan" title="Plan" to="/vp/plan" icon={<Book />} />
+					<NavItem key="/vp/report" title="Report" to="/vp/report" icon={<BarChartOutlinedIcon />} />
+					<NavItem key="/vp/schedule" title="Schedule" to="/vp/schedule" icon={<CalendarMonth />} />
+					<NavItem key="/vp/generate-plan" title="Generate Plan" to="/vp/generate-plan" icon={<Chat />} />
 				</Fragment>
 			),
 		},
