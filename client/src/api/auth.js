917db13317d163ab878594cd5bb19c2197b17c3a
Paulos Teshome Hailemeskel <paulman7792@gmail.com>
2025-01-22 08:36:00 +0300

    Update auth.js

--- client/src/api/auth.js
+++ client/src/api/auth.js
@@ -6,17 +6,21 @@ const useAuthApi = () => {
 	};
 
 	const logoutUser = (data) => {
-		return axios.post('/users/logout', data, { withCredentials: true }).then((res) => res.data);
+		return axios.get('/users/logout', { withCredentials: true }).then((res) => res.data);
 	};
 
 	const registerUser = (data) => {
 		return axios.post('/users/signup', data, { withCredentials: true }).then((res) => res.data);
 	};
+
+	const getAllUsers = (data) => {
+		return axios.get('/users/').then((res) => res.data);
+	};
 	// const refreshToken = (data) => {
 	// 	return axios.post('/auth/refresh', data, { withCredentials: true }).then((res) => res.data);
 	// };
 
-	return { loginUser, logoutUser, registerUser };
+	return { loginUser, logoutUser, registerUser, getAllUsers };
 };
 
 export default useAuthApi;
