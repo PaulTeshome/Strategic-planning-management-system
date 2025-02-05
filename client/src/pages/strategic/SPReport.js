e97be428a859cc527b77c80b7a0c7027a8387df1
Paulos Teshome Hailemeskel <paulman7792@gmail.com>
2025-02-05 07:37:15 +0300

    view report

--- client/src/pages/strategic/SPReport.js
+++ client/src/pages/strategic/SPReport.js
@@ -1,6 +1,6 @@
 import { useTheme } from '@emotion/react';
 import { Button, Grid2, Stack, TextField, Typography } from '@mui/material';
-import React, { useState } from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
 import { tokens } from '../../theme';
 import { useFormik } from 'formik';
 import SelectComponent from '../../components/form/SelectComponent';
@@ -10,21 +10,58 @@ import { mockReport } from '../../components/data/mockData';
 import { CheckCircle } from '@mui/icons-material';
 import ConfirmationModal from '../../components/modals/ConfirmationModal';
 import { getDepartmentByRole } from '../../utils/getDepartmentByRole';
+import { useParams } from 'react-router-dom';
+import useReportApi from '../../api/report';
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
+import toast from 'react-hot-toast';
 
 function SPReport() {
 	const theme = useTheme();
 	const colors = tokens(theme.palette.mode);
 	const [confirmOpen, setConfirmOpen] = useState(false);
+	const queryClient = useQueryClient();
+
+	const [rows, setRows] = useState([]);
+	const date = new Date();
+
+	const { report_id } = useParams();
+	const { getReport, getBy, updateReport } = useReportApi();
+
+	const [selectedPlan, setSelectedPlan] = useState({ _id: report_id });
+
+	const getReportQuery = useQuery({
+		queryKey: ['report', report_id, 'submitted'],
+		enabled: report_id !== undefined && report_id !== null,
+		queryFn: getReport,
+		staleTime: 1000 * 60 * 5,
+		// retry: false,
+	});
+
+	useEffect(() => {
+		if (getReportQuery.status === 'error') {
+			// console.log('🚀 ~ Patients ~ getReportQuery.error:', getReportQuery.error);
+			toast.error(
+				getReportQuery.error?.response?.data?.message || getReportQuery.error.message || 'Error getting plans'
+			);
+		}
+	}, [getReportQuery.status, getReportQuery.error]);
+
+	useMemo(() => {
+		if (getReportQuery.status === 'success') {
+			console.log('🚀 ~ useMemo ~ getReport', getReportQuery.data);
+			const patientList = getReportQuery.data?.data?.data[0]?.planData || [];
+			setRows([...patientList]);
+		}
+	}, [getReportQuery.status, getReportQuery.data]);
 
 	const closeConfirm = () => {
 		setConfirmOpen(false);
 	};
-	const handleApprove = () => {
-		console.log('🚀 ~ handleApprove ~ first:', values);
-	};
 
-	const handleSearch = () => {};
-	const date = new Date();
+	const handleSearch = (values) => {
+		// const { year, department } = values;
+		getByYearnDeptQ.refetch();
+	};
 
 	const { values, errors, handleSubmit, handleBlur, handleChange, touched } = useFormik({
 		initialValues: {
@@ -34,6 +71,62 @@ function SPReport() {
 		validationSchema: vpReportSchema,
 		onSubmit: handleSearch,
 	});
+
+	const getByYearnDeptQ = useQuery({
+		queryKey: ['report', values.year, values.department, 'submitted'],
+		enabled: false,
+		queryFn: getBy,
+		staleTime: 1000 * 60 * 5,
+		retry: false,
+	});
+
+	useEffect(() => {
+		if (getByYearnDeptQ.status === 'error') {
+			// console.log('🚀 ~ Patients ~ getByYearnDeptQ.error:', getByYearnDeptQ.error);
+			toast.error(
+				getByYearnDeptQ.error?.response?.data?.message || getByYearnDeptQ.error.message || 'Error getting plans'
+			);
+		}
+	}, [getByYearnDeptQ.status, getByYearnDeptQ.error]);
+
+	useMemo(() => {
+		if (getByYearnDeptQ.status === 'success') {
+			// console.log('🚀 ~ useMemo ~ getReport', getByYearnDeptQ.data);
+			const planData = getByYearnDeptQ.data?.data?.data[0]?.planData || [];
+			const planInfo = getByYearnDeptQ.data?.data?.data[0];
+			console.log('🚀 ~ useMemo ~ planInfo:', planInfo);
+			setSelectedPlan({ ...planInfo });
+			setRows([...planData]);
+		}
+	}, [getByYearnDeptQ.status, getByYearnDeptQ.data]);
+
+	const updateReportMut = useMutation({
+		mutationFn: updateReport,
+		mutationKey: ['updateReport'],
+		onSuccess: (response) => {
+			// // console.log('🚀 ~ AddNewPatient ~ response:', response);
+			toast.success('Update ' + response.status);
+			queryClient.invalidateQueries({ queryKey: ['reports'] });
+			// setUpdate(false);
+			getReportQuery.refetch();
+		},
+		onError: (error) => {
+			// // console.log('🚀 ~ AddNewPatient ~ error:', error);
+
+			toast.error(error.response.data.message || error.response.statusText);
+		},
+	});
+
+	const handleApprove = (values) => {
+		console.log('🚀 ~ handleApprove ~ first:', values);
+		const data = { report_id: selectedPlan._id, status: 'requested' };
+		if (selectedPlan._id === undefined || selectedPlan._id === null) {
+			toast.error('Please select a report for approval request!');
+			return;
+		}
+		updateReportMut.mutate(data);
+		// console.log('🚀 ~ handleApprove ~ data:', data);
+	};
 
 	return (
 		<Stack
@@ -93,7 +186,7 @@ function SPReport() {
 						onChange={handleChange}
 						onBlur={handleBlur}
 						options={[
-							{ value: 'all', label: 'All' },
+							// { value: 'all', label: 'All' },
 							{ value: 'av', label: getDepartmentByRole('av') },
 							{ value: 'vpo', label: getDepartmentByRole('vpo') },
 							{ value: 'rv', label: getDepartmentByRole('rv') },
@@ -138,19 +231,75 @@ function SPReport() {
 			</Grid2>
 
 			<ViewReportTable
+				// columns={[
+				// 	{ name: 'Number', colSpan: 1 },
+				// 	{ name: 'Strategic Goals , Main Activities, Detail functions and KPIs', colSpan: 1 },
+				// 	{ name: 'Weights', colSpan: 1 },
+				// 	{ name: 'Measurements', colSpan: 1 },
+				// 	{ name: `Previous year(${values.year - 1}) value`, colSpan: 1 },
+				// 	{ name: `This year(${values.year}) Goal`, colSpan: 1 },
+				// 	{ name: 'Quarter 1 and 2 goal', colSpan: 1 },
+				// 	{ name: 'Quarter 1 and 2 implementation', colSpan: 1 },
+				// 	{ name: 'Quarter 1 and 2 completeness by percentage', colSpan: 1 },
+				// 	{ name: 'Department', colSpan: 1 },
+				// ]}
+
+				topColumns={[
+					{ name: '', colSpan: 10 },
+					{ name: 'Quarter 1', colSpan: 2 },
+					{ name: 'Quarter 2', colSpan: 2 },
+					{ name: 'Quarter 3', colSpan: 2 },
+					{ name: 'Quarter 4', colSpan: 2 },
+				]}
 				columns={[
 					{ name: 'Number', colSpan: 1 },
-					{ name: 'Strategic Goals , Main Activities, Detail functions and KPIs', colSpan: 1 },
+					{ name: 'Titles', colSpan: 5 },
 					{ name: 'Weights', colSpan: 1 },
 					{ name: 'Measurements', colSpan: 1 },
 					{ name: `Previous year(${values.year - 1}) value`, colSpan: 1 },
 					{ name: `This year(${values.year}) Goal`, colSpan: 1 },
-					{ name: 'Quarter 1 and 2 goal', colSpan: 1 },
-					{ name: 'Quarter 1 and 2 implementation', colSpan: 1 },
-					{ name: 'Quarter 1 and 2 completeness by percentage', colSpan: 1 },
+					{
+						name: 'Plan',
+						colSpan: 1,
+					},
+					{
+						name: 'Progress',
+						colSpan: 1,
+					},
+
+					{
+						name: 'Plan',
+						colSpan: 1,
+					},
+					{
+						name: 'Progress',
+						colSpan: 1,
+					},
+
+					{
+						name: 'Plan',
+						colSpan: 1,
+					},
+					{
+						name: 'Progress',
+						colSpan: 1,
+					},
+
+					{
+						name: 'Plan',
+						colSpan: 1,
+					},
+					{
+						name: 'Progress',
+						colSpan: 1,
+					},
+					{
+						name: 'Performance',
+						colSpan: 0.5,
+					},
 					{ name: 'Department', colSpan: 1 },
 				]}
-				rows={mockReport}
+				rows={rows}
 			/>
 		</Stack>
 	);
